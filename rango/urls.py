d045f40b3f3455b0c6d37e09d6a0ce68cc0a8a97
Aimee Mackin <2898220m@student.gla.ac.uk>
2025-02-07 11:55:16 +0000

    chapter 3

---
+++ rango/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rango import views
+
+app_name='rango'
+
+urlpatterns = [
+    #path(string to match, what to call if string matches, (optional) name -> convenient for referencing the view)
+    path('', views.index, name='index')
+]
