973d33e007c612f753a579e22dee92eeb59519a7
Aimee Mackin <2898220m@student.gla.ac.uk>
2025-02-11 14:12:43 +0000

    chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     #path(string to match, what to call if string matches, (optional) name -> convenient for referencing the view)
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('category/<slug:category_name_slug>/', views.show_category, name='show_category')
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
